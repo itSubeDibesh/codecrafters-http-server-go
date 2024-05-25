05866f2188722bb6d4bc4c6254307030026c5f8d
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2024-05-25 18:50:56 +0545

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -8,17 +8,44 @@ import (
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Println("Initializing server...")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	const PORT = 4221
+
+	// Used for inbound connections -> We only use it as we are server not client
+	listener, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%v", PORT))
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		fmt.Printf("Failed to bind to port %v\n", PORT)
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer listener.Close()
+	fmt.Printf("Listening on port %v\n", PORT)
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+
+		handelClientConnection(connection)
 	}
+}
+
+func handelClientConnection(connection net.Conn) {
+	defer connection.Close()
+	buffer := make([]byte, 1024)
+	_, err := connection.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading request: ", err.Error())
+		return
+	}
+
+	_, err = connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error writing response: ", err.Error())
+		return
+	}
+
 }
